816aa473a6738a190e8716f2ef344dc50320f33c
cvargsh <carlos.vargas.sistemas@gmail.com>
2021-11-06 10:59:32 -0500

    Primer commit

---
+++ src/app/answer-button/answer-button.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit, Output, OnChanges, EventEmitter, SimpleChanges } from '@angular/core';
+
+
+@Component({
+  selector: 'app-answer-button',
+  templateUrl: './answer-button.component.html',
+  styleUrls: ['./answer-button.component.css']
+})
+export class AnswerButtonComponent implements OnInit, OnChanges {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("ngOnInit ejecutado")
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges ejecutado", changes)
+  }
+
+  @Input() answer: string | undefined
+  @Output() buttonSelected: EventEmitter<any> = new EventEmitter()
+
+  variablecualquiera = 0
+
+  selectAnswer(){
+    this.buttonSelected?.emit(this.answer)
+    this.variablecualquiera++
+    console.log("NUMERO", this.variablecualquiera)
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamePageComponent } from './game-page/game-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ScorePageComponent } from './score-page/score-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginPageComponent
+  },
+  {
+    path: "game",
+    component: GamePageComponent
+  },
+  {
+    path: "score",
+    component: ScorePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
